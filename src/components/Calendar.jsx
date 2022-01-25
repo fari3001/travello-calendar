6fb4467ad1b3e3f5ac10a5a6dd7d88b46f8a35d1
Farhaan <fari3001_14@hotmail.com>
2022-01-25 23:45:17 +1000

    Edits to the modal body

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
 import {Modal, Button} from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Calendar from 'react-calendar'
-import "../components/styles/calendar.scss"
+import Calendar from 'react-calendar';
+import "../components/styles/calendar.scss";
 
-export default function Calender() {
+export default function Calender(props) {
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+    const [date, setDate] = useState(new Date());
 
-    const onChange = (date) => {
-        console.log(date.toString());
-      };
+    const onChange = date => {
+        setDate(date);
+    }
+    
+
 
     return (
         <div className="container">
@@ -28,16 +31,18 @@ export default function Calender() {
                     <div className="modal-wrapper">
                         <div className="modal-adult">Adult</div>
                         <div className="modal-button-wrapper">
-                            <button />1
-                            <button />
+                            <button className="modal-body-buttons-left"/>
+                            <input className="number-input" min="1" name="quantity" value="1" type="number" id="adultValue"></input>
+                            <button className="modal-body-buttons-right"/>
                         </div>
                         <div className="modal-children-wrapper">
                             <div className="modal-children">Children</div>
                             <div className="modal-children-years">3-15 Years</div>
                         </div>
                         <div className="modal-button-wrapper">
-                            <button />1
-                            <button />
+                            <button className="modal-body-buttons-left"/>
+                            <input className="number-input" min="0" name="quantity" value="0" type="number"></input>
+                            <button className="modal-body-buttons-right"/>
                         </div>
                         
                     </div>
@@ -45,9 +50,14 @@ export default function Calender() {
                 <Modal.Footer>
                     <div className="modal-footer-wrapper">
                         <div className="modal-calendar">
-                        <Calendar />
+                        <Calendar onChange={onChange} date={date} hover />
                         </div>
-                        <div className="modal-packages"></div>
+                        <div className="modal-packages">
+                            <div className="modal-date-filter">
+                                <div className="modal-date">12 September 2021</div>
+                                <div className="modal-filter"></div>
+                            </div>
+                        </div>
                     </div>
                 </Modal.Footer>
             </Modal>
